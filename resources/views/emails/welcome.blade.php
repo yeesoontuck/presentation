3bc68252cecd526711b79ec877352d7ef82b15df
Yee Soon Tuck <soontuck.yee@aptsys.com.sg>
2017-02-05 12:52:58 +0800

    created the view file as the email content (HTML)

---
+++ resources/views/emails/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome to Example.com</title>
+</head>
+<body>
+    <h5>Hello, {{ $user->name }}</h5>
+
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, dicta.</div>
+    <div>Facilis ducimus ab assumenda, nulla illo harum sit cumque autem?</div>
+    <div>Dicta fugit quos laborum soluta magni, perferendis ullam non inventore?</div>
+    <div>Sequi at et, ipsam illum. Quos sed minus deserunt error.</div>
+    <div>Fugit fuga aspernatur magnam sint laboriosam fugiat eos deleniti nisi!</div>
+</body>
+</html>
