5567463a1ecac9f6a17d4eb45d651c8132c9f75a
Yee Soon Tuck <soontuck.yee@aptsys.com.sg>
2017-02-05 12:40:50 +0800

    initial commit

---
+++ resources/views/emails/~welcome.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+</head>
+<body>
+    <img src="{{ $message->embed(public_path() . '/images/welcome.jpg') }}" alt="">
+
+    <h1>Welcome {{ $user->name }}</h1>
+
+    <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Necessitatibus inventore consequatur reprehenderit a, deleniti commodi explicabo aliquam. Tempore est distinctio, sed impedit vero ducimus error atque, nostrum praesentium, odit possimus.</div>
+    <div>Accusamus voluptatem molestias provident deleniti quidem laboriosam, quas sapiente laborum quia culpa. Est, sequi perspiciatis illum eos. Ea praesentium itaque quasi, cumque, ut eos! In ratione, explicabo nihil. Officia, minus!</div>
+    <div>Consectetur amet doloribus possimus autem ea nesciunt nihil expedita voluptate, animi harum illo quam pariatur obcaecati exercitationem asperiores fugiat recusandae facere mollitia est libero iure debitis. Tenetur doloribus at obcaecati?</div>
+    <div>Provident praesentium alias esse quia maxime ab aspernatur facere sit culpa, quidem voluptate similique, facilis fugit! Eveniet ab repellat, pariatur magnam hic delectus magni, assumenda dolores dolor, deserunt laborum ducimus.</div>
+    <div>Alias voluptas ipsa possimus officiis repellendus at dolores, modi deleniti dolorem inventore voluptatibus quidem impedit assumenda cum pariatur. Omnis rerum dolore inventore labore tenetur illum ipsa ad nisi rem porro.</div>
+</body>
+</html>
