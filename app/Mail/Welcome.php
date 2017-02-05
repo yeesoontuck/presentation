c51d007e6c1859398c0cc64030da8186df6f5a46
Yee Soon Tuck <soontuck.yee@aptsys.com.sg>
2017-02-05 13:04:32 +0800

    inject a User object into Welcome mailable Test route

--- app/Mail/Welcome.php
+++ app/Mail/Welcome.php
@@ -7,18 +7,22 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
+use App\User;
+
 class Welcome extends Mailable
 {
     use Queueable, SerializesModels;
+
+    public $user;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        //
+        $this->user = $user;
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,11 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
+
+Route::get('/test', function()
+{
+    $user = App\User::find(1);
+
+    \Mail::to($user->email)
+        ->send(new App\Mail\Welcome($user));
+});
